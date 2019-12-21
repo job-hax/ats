43e7b4aa0fceb1b385d6ea12e7f49821c44d3ace
sakomws <smammadov@opengov.com>
2019-12-20 17:17:11 -0800

    Init commit

---
+++ frontend/src/config/example.js
@@ -0,0 +1,13 @@
+export const IS_MOCKING = false;
+export const IS_RECAPTCHA_ENABLED = false;
+
+export const googleClientId = '';
+export const googleApiKey = '';
+
+export const jobHaxClientId = '';
+export const jobHaxClientSecret = '';
+
+export const reCaptchaV3SiteKey = '';
+
+export const linkedInClientId = '';
+export const linkedInClientSecret = '';
